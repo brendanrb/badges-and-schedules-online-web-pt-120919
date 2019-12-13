c2071bd4030e7c30a547984d6860266ff31f9842
Brendan R Borgers <brendanborgers@gmail.com>
2019-12-13 01:07:28 +0000

    Automatically backed up by Learn

--- conference_badges.rb
+++ conference_badges.rb
@@ -25,6 +25,7 @@ def printer(attendees)
   puts batch_badge_creator(attendees)[2]
   puts batch_badge_creator(attendees)[3]
   puts batch_badge_creator(attendees)[4]
+  puts batch_badge_creator(attendees)[5]
 
 end
 
