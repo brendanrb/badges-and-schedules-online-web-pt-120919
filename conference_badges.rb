fe78b813b785e5ae72e98c82705c5d5c203f7bb3
Brendan R Borgers <brendanborgers@gmail.com>
2019-12-13 00:25:51 +0000

    Automatically backed up by Learn

--- conference_badges.rb
+++ conference_badges.rb
@@ -3,20 +3,6 @@ def badge_maker(name)
   
 end
 
-def batch_badge_creator(attendees)
-  new_array = []
-  attendees.each {|a| new_array << "Hello, my name is #{a}."}
-    return new_array
-
-end
-
-def assign_rooms(attendees)
-
-  new_array = []
-  def badge_maker(name)
-  return "Hello, my name is #{name}."
-  
-end
 
 def batch_badge_creator(attendees)
   new_array = []
