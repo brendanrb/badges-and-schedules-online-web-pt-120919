fecaee262a904615056a05622870aae9460d3321
Brendan R Borgers <brendanborgers@gmail.com>
2019-12-13 00:11:33 +0000

    Automatically backed up by Learn

--- conference_badges.rb
+++ conference_badges.rb
@@ -28,7 +28,7 @@ end
 def assign_rooms(attendees)
   new_array = []
 attendees.each.with_index(1) do |attendee, index|
-    new_array << "Hello, #{attendee}!. You are in room #{index}."
+    new_array << "Hello, #{attendee}! You are in room #{index}."
 end
 return new_array
 end
